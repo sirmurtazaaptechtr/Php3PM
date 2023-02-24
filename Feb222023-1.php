b8287a888202e54c20a560485e17f637f1d2e288
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-02-24 15:30:00 +0500

    22-Feb-2023

---
+++ Feb222023-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My Php Project</h1>
+    <?php
+        $msg = "My name is Hussain";
+        echo "this is my php webproject";
+    ?>
+    <h2>This is another heading</h2>
+
+    <h3><?php echo "this is heading 3";?></h3>
+    <h3><?php echo $msg;?></h3>
+    
+</body>
+</html>
