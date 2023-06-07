4bc1e4c3499657ad8519baaf0724982964fbb91c
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-06-07 17:32:29 +0500

    07-Jun-2023

--- MySQL/Jun072023-1/customers3.php
+++ MySQL/Jun072023-1/customers3.php
@@ -197,6 +197,7 @@ $connection = new mysqli($servername, $username, $password, $database);
             </div>
         </form>
     </div>
+    <div class="table-responsive">
     <table class="table table-dark table-hover">
         <thead>
             <tr>
@@ -247,6 +248,7 @@ $connection = new mysqli($servername, $username, $password, $database);
             ?>
         </tbody>
     </table>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
